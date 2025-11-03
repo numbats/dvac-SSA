9a7ac45172f1ac85d66bb2a1d24fb64b2cc46b58
Kate Saunders <kate.saunders@monash.edu>
2025-11-03 19:35:30 +1100

    Week 1 + small website tweaks

---
+++ helpers/Unit_reminders.R
@@ -0,0 +1,6 @@
+Unit Set Up:
+* Moodle (goes live at the start of SSA not the semester dates!!!)
+* Update unit urls in _quarto.yml for the website links
+* Update course_info for web page creating start dates, lecture names etc
+* Review the index.qmd file for the webpage with the unit details  
+  
